d8cc640f26c2ed7fe44ef1892e43eb70495da379
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-02-29 17:42:55 +0500

    Create simple prompt project

---
+++ Promt Engineering with Kernel/Prompts/N01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+// Create configurations
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets<Program>();
+var configuration = configurationBuilder.Build();
+
+// Create kernel builder
+var kernelBuilder = Kernel.CreateBuilder();
+
+
+Console.WriteLine(configuration["Hello:Hi"]!);
+
+
+
+// Add OpenAI connector
+kernelBuilder.AddOpenAIChatCompletion(modelId: "gpt-3.5 turbo", apiKey: configuration["OpenAiApiSettings:ApiKey"]!);
+
+// Build kernel
+var kernel = kernelBuilder.Build();
+
+// Create simple prompt
+Console.Write("Your request: ");
+var request = Console.ReadLine();
+var prompt = $"What is the intent of this request? {request}";
+
+// Display request intent
+Console.WriteLine(await kernel.InvokePromptAsync(prompt));
